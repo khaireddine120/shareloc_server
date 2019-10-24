545bf6f56ba5a6d74dc8b3a03d371ff1f234e5a9
khairy <khaireddine.rezgui@machinestlak.com>
2019-10-24 12:38:04 +0100

    init project

---
+++ src/main/java/com/shareloc/sharelocserver/SharelocServerApplication.java
@@ -0,0 +1,13 @@
+package com.shareloc.sharelocserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharelocServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharelocServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shareloc/sharelocserver/SharelocServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareloc.sharelocserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharelocServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
